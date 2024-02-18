3011c14c401a3b1980a62df76470695faaad51ea
ZaK3939 <tsu.aria.kalei@gmail.com>
2024-02-18 23:45:51 +0900

    feat; update

--- app/api/start/route.ts
+++ app/api/start/route.ts
@@ -6,32 +6,34 @@ import { getAddressButtons } from '../../lib/addresses';
 import { allowedOrigin } from '../../lib/origin';
 import { getFrameHtml } from '../../lib/getFrameHtml';
 import { TriggerResponse } from '../../lib/types';
-import { mintResponse } from '../../lib/responses';
+import { errorResponse, mintResponse } from '../../lib/responses';
 import { retryableApiPost } from '../../lib/retry';
+import { error } from 'console';
 
 async function getResponse(req: NextRequest): Promise<NextResponse> {
   const body: FrameRequest = await req.json();
   const { isValid, message } = await getFrameMessage(body, {
     neynarApiKey: process.env.NEYNAR_API_KEY,
   });
+
   if (message?.button === 2 && isValid && allowedOrigin(message)) {
-    const address = message.interactor.verified_accounts[0].toLowerCase();
-    const result = await retryableApiPost<TriggerResponse>(PHI_GRAPH, {
-      query: conditionTrigger(address),
-    });
-    console.log('result', result);
-    if (result && result.data?.conditionTrigger.success) {
-      const buttons = getAddressButtons(message.interactor);
-      return new NextResponse(
-        getFrameHtml({
-          buttons,
-          image: `${NEXT_PUBLIC_URL}/api/images/unclaimed`,
-          post_url: `${NEXT_PUBLIC_URL}/api/confirm`,
-        }),
-      );
-    } else {
-      return mintResponse();
+    for (const address of message?.interactor.verified_accounts) {
+      const result = await retryableApiPost<TriggerResponse>(PHI_GRAPH, {
+        query: conditionTrigger(address),
+      });
+      if (!result.data?.conditionTrigger.success) {
+        return errorResponse();
+      }
     }
+
+    const buttons = getAddressButtons(message.interactor);
+    return new NextResponse(
+      getFrameHtml({
+        buttons,
+        image: `${NEXT_PUBLIC_URL}/api/images/unclaimed`,
+        post_url: `${NEXT_PUBLIC_URL}/api/confirm`,
+      }),
+    );
   } else return new NextResponse('Unauthorized', { status: 401 });
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,6 @@ import type { Metadata } from 'next';
 import { NEXT_PUBLIC_URL } from './config';
 
 export async function generateMetadata(): Promise<Metadata> {
-  // const { name } = await getCollection();
-  const name = 'zak3939';
   const frameMetadata = getFrameMetadata({
     buttons: [
       {
@@ -21,11 +19,11 @@ export async function generateMetadata(): Promise<Metadata> {
   });
 
   return {
-    title: name,
-    description: "Check if you're eligible for a free mint",
+    title: 'Philand',
+    description: "Check if you're eligible for a mint",
     openGraph: {
-      title: name,
-      description: "Check if you're eligible for a free mint",
+      title: 'Philand',
+      description: "Check if you're eligible for a mint",
       images: [`${NEXT_PUBLIC_URL}/api/images/start`],
     },
     other: {
@@ -34,44 +32,6 @@ export async function generateMetadata(): Promise<Metadata> {
     },
   };
 }
-
-// const frameMetadata = getFrameMetadata({
-//   buttons: [
-//     {
-//       label: 'Story time!',
-//     },
-//     {
-//       action: 'link',
-//       label: 'Link to Google',
-//       target: 'https://www.google.com',
-//     },
-//     {
-//       label: 'Redirect to pictures',
-//       action: 'post_redirect',
-//     },
-//   ],
-//   image: {
-//     src: `${NEXT_PUBLIC_URL}/park-3.png`,
-//     aspectRatio: '1:1',
-//   },
-//   input: {
-//     text: 'Tell me a boat story',
-//   },
-//   postUrl: `${NEXT_PUBLIC_URL}/api/frame`,
-// });
-
-// export const metadata: Metadata = {
-//   title: 'zizzamia.xyz',
-//   description: 'LFG',
-//   openGraph: {
-//     title: 'zizzamia.xyz',
-//     description: 'LFG',
-//     images: [`${NEXT_PUBLIC_URL}/park-1.png`],
-//   },
-//   other: {
-//     ...frameMetadata,
-//   },
-// };
 
 export default async function Page() {
   return <></>;
