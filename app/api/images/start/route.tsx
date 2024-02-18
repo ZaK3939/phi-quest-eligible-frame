21d1ad9bea2e8b835624ae635a3e7e756f18bec8
ZaK3939 <tsu.aria.kalei@gmail.com>
2024-02-18 22:52:16 +0900

    feat; update

--- app/api/images/start/route.tsx
+++ app/api/images/start/route.tsx
@@ -1,10 +1,15 @@
 import { ImageResponse } from 'next/og';
 import { Card } from '../../../components/Card';
-import { CARD_DIMENSIONS } from '../../../config';
+import { CARD_DIMENSIONS, NEXT_PUBLIC_URL } from '../../../config';
 
 export async function GET() {
   return new ImageResponse(
-    <Card message="Claim free with Warpcast active badge or mint with Warps." />,
+    (
+      <Card
+        message="Claim free with Warpcast active badge or mint with Warps."
+        // image={`${NEXT_PUBLIC_URL}/phi.png`}
+      />
+    ),
     CARD_DIMENSIONS,
   );
 }
