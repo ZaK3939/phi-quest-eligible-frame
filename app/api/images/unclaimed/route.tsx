c25b8ea4bd7c653694d16620f9be967f84578364
ZaK3939 <tsu.aria.kalei@gmail.com>
2024-02-19 18:13:32 +0900

    feat; update

--- app/api/images/unclaimed/route.tsx
+++ app/api/images/unclaimed/route.tsx
@@ -4,7 +4,7 @@ import { CARD_DIMENSIONS, NEXT_PUBLIC_URL } from '../../../config';
 
 export async function GET() {
   return new ImageResponse(
-    <Card message="Your eligible check is in the queue." image={`${NEXT_PUBLIC_URL}/phi.png`} />,
+    <Card message="Your eligible check is in the queue." image={`${NEXT_PUBLIC_URL}/phi404.png`} />,
     CARD_DIMENSIONS,
   );
 }
