bf844fe4e15c02b849a0cda16b14fa5b37701320
ZaK3939 <tsu.aria.kalei@gmail.com>
2024-02-19 22:50:54 +0900

    feat; update

--- app/lib/getFrameHtml.ts
+++ app/lib/getFrameHtml.ts
@@ -4,9 +4,9 @@ export function getFrameHtml(frameMetadata: FrameMetadataType) {
   const html = getFrameHtmlResponse(frameMetadata);
 
   const extraTags = [
-    '<meta property="og:title" content="Farcaster: Horse">',
-    '<meta property="og:description" content="Farcaster Protocol Release">',
-    '<meta property="og:image" content="https://mint.farcaster.xyz/api/images/start">',
+    '<meta property="og:title" content="phi: Quest">',
+    '<meta property="og:description" content="phi Protocol Checker">',
+    '<meta property="og:image" content="https://phi-quest-eligible-frame.vercel.app/api/images/start">',
     '<meta property="fc:frame:image:aspect_ratio" content="1:1" />',
   ];
   // hack: remove close tags, add aspect ratio and required OG tags
