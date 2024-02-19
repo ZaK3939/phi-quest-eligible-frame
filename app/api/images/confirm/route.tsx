f61fb5734625cc5fc108ccce01b24df7110633c9
ZaK3939 <tsu.aria.kalei@gmail.com>
2024-02-19 18:06:27 +0900

    feat; update

--- app/api/images/confirm/route.tsx
+++ app/api/images/confirm/route.tsx
@@ -11,7 +11,7 @@ export async function GET(req: NextRequest) {
   return new ImageResponse(
     (
       <Card
-        message={`Mint ${tokenId} to ${address}?`}
+        message={`🎁　You are eligible to Mint.`}
         image={`${NEXT_PUBLIC_URL}/quest/${tokenId}.png`}
       />
     ),
