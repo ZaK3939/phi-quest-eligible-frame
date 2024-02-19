99971d2f592ce5321019e3b08842578375b1746f
ZaK3939 <tsu.aria.kalei@gmail.com>
2024-02-19 12:08:43 +0900

    feat; update

--- app/api/relay/route.ts
+++ app/api/relay/route.ts
@@ -4,9 +4,6 @@ import { NEXT_PUBLIC_URL, PHI_GRAPH, queryForLand } from '../../config';
 import { getAddressButtons } from '../../lib/addresses';
 import { allowedOrigin } from '../../lib/origin';
 import { getFrameHtml } from '../../lib/getFrameHtml';
-import { errorResponse, mintResponse } from '../../lib/responses';
-import { LandResponse } from '../../lib/types';
-import { retryableApiPost } from '../../lib/retry';
 
 async function getResponse(req: NextRequest): Promise<NextResponse> {
   const body: FrameRequest = await req.json();
@@ -16,25 +13,17 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
 
   if (isValid && allowedOrigin(message)) {
     const address = message.interactor.verified_accounts[0].toLowerCase();
-    const result = await retryableApiPost<LandResponse>(PHI_GRAPH, {
-      query: queryForLand(address),
-    });
-
-    if (result.data && result.data.philandList.data) {
-      return new NextResponse(
-        getFrameHtml({
-          buttons: [
-            {
-              label: '🔄 Check status',
-            },
-          ],
-          post_url: `${NEXT_PUBLIC_URL}/api/check`,
-          image: `${NEXT_PUBLIC_URL}/api/images/check`,
-        }),
-      );
-    } else {
-      return mintResponse();
-    }
+    return new NextResponse(
+      getFrameHtml({
+        buttons: [
+          {
+            label: '🔄 Check status',
+          },
+        ],
+        post_url: `${NEXT_PUBLIC_URL}/api/check`,
+        image: `${NEXT_PUBLIC_URL}/api/images/check`,
+      }),
+    );
   } else return new NextResponse('Unauthorized', { status: 401 });
 }
 
